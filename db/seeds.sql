95edfac0983a2ad2bd9b7e10173a82b22d1340a0
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2020-11-23 17:57:47 -0500

    Created server.js, schema, and seeds files and put in code to require express, connect to the server and to create a table. Still need to work on the devoured boolean on table. Added necessary files and organized them properly. Added cute burger image

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Hamburger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
+
+-- Have to implement devoured
