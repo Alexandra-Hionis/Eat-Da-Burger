5fa7357289d99e75bed1e8dd68afc3b72dc4329e
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2020-11-24 16:45:16 -0500

    added delete function. Made a lot of burgers in the app and it looks sloppy

--- public/assets/js/burgers_script.js
+++ public/assets/js/burgers_script.js
@@ -45,5 +45,17 @@ $(function() {
         }
       );
     });
+
+    $(".deleteburger").on("click", function(event) {
+      event.preventDefault();
+
+      var id = $(this).data("id");
+
+      // Send the DELETE request.
+      $.ajax({
+          type: "DELETE",
+          url: "/api/burgers/" + id
+      }).then(location.reload());
+  });
   });
   
